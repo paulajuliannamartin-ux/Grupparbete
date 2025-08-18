78b6dea876a3f5598604b3b2ba17e158a0179b50
paulajuliannamartin-ux <paula.julianna.martin@gmail.com>
2025-08-18 13:28:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ function showPage(pageId) {
   const pages = document.querySelectorAll('.content');
   pages.forEach(page => page.classList.remove('active'));
   document.getElementById(pageId).classList.add('active');
+
+  // Stänger menyn på mobil efter klick
+  document.querySelector("nav ul").classList.remove("show");
 }
+
+function toggleMenu() {
+  document.querySelector("nav ul").classList.toggle("show");
+}
+
+
